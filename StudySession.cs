3925a663919e2b098a59e01c67f6a133017e4ed2
Adeesha Peiris <wap.rulz@gmail.com>
2024-08-03 21:18:48 +0530

    Add project files.

---
+++ StudySession.cs
@@ -0,0 +1,32 @@
+﻿namespace SPTModels
+{
+    public class StudySession
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int Duration { get; set; }
+        public int ModuleId { get; set; }
+        public StudyLevel StudyLevel { get; set; }
+
+		public StudySession()
+		{
+		}
+
+		public StudySession(int id, DateTime date, int duration, int moduleId, StudyLevel studyLevel)
+        {
+            Id = id;
+            Date = date;
+            Duration = duration;
+            ModuleId = moduleId;
+            StudyLevel = studyLevel; 
+        }
+    }
+
+    public enum StudyLevel
+    {
+        Beginner,
+        Intermediate,
+        Advanced,
+        Expert
+    }
+}
---
+++ User.cs
@@ -0,0 +1,27 @@
+﻿namespace SPTModels;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string PasswordHash { get; set; }
+    public string Salt { get; set; }
+
+    public User()
+    {
+    }
+
+    public User(int id, string username, string passwordHash, string salt)
+    {
+        Id = id;
+        Username = username;
+        PasswordHash = passwordHash;
+        Salt = salt;
+    }
+
+    public class UserDTO
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
